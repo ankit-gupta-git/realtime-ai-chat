b1dd7656e56c16e14a4ec0910045490341f6a23f
Ankit Gupta <ankitkumargupta752@gmail.com>
2025-10-23 03:49:58 +0530

    backend url updated

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    return io('http://localhost:4600');
+    return io('https://realtime-ai-chat.onrender.com/'); // <- use HTTPS
 }
+
